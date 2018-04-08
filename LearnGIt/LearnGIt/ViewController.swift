4eab3934336a46c1126e08eac088cf83898870dd
git <1083229687@qq.com>
2018-04-08 10:16:24 +0800

    add a project

---
+++ LearnGIt/LearnGIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnGIt
+//
+//  Created by maiya on 2018/4/8.
+//  Copyright © 2018 maiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
